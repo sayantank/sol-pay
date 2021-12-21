0f2beee900d7d25b5b9c6251ad1ea9847f0efc88
Sayantan Karmakar <sayantankarmakar@outlook.com>
2021-12-21 10:56:36 +0530

    added idl

--- app/hooks/useWorkspace.tsx
+++ app/hooks/useWorkspace.tsx
@@ -3,7 +3,7 @@ import { Connection } from "@solana/web3.js";
 import { Provider, Program, Idl } from "@project-serum/anchor";
 import { PublicKey } from "@solana/web3.js";
 
-import idl from "../../target/idl/sol_pay.json";
+import idl from "../idls/sol_pay.json";
 
 const PROGRAM_ID = new PublicKey(idl.metadata.address);
 
